a84847d4b4d41e42d593c439bac3952a3f23c96e
chintan chawla <chintan14987@gmail.com>
2022-03-27 10:58:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Overview from "./Overview/Overview.js";
+import { Route, Routes } from "react-router-dom";
+import Mission from "./Mission/Mission";
+import Values from "./Values/Values";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Overview />
     </div>
   );
-}
+};
 
 export default App;
